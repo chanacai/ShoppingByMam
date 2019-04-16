d4fdbc2ff7db29d31ea1c3a8c46790106d418912
sam <twinsamson@gmail.com>
2019-04-16 15:34:47 +0700

    back ok

--- ShoppingByMam/RegisterViewController.swift
+++ ShoppingByMam/RegisterViewController.swift
@@ -14,17 +14,23 @@ class RegisterViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    } //Main Method
+
+    
+    
+    @IBAction func backBotton(_ sender: UIBarButtonItem) {
+        
+        performSegue(withIdentifier: "BackToAuthen", sender: nil)
+        
+    }
+ 
+    
+    @IBAction func uploadButton(_ sender: UIBarButtonItem) {
     }
     
+    
+    
+    
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
+} //Main Class
