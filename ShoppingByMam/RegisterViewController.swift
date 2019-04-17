f566b2a3191daa023a96f79d810c17da75b1aae1
sam <twinsamson@gmail.com>
2019-04-17 10:36:56 +0700

    check space success

--- ShoppingByMam/RegisterViewController.swift
+++ ShoppingByMam/RegisterViewController.swift
@@ -10,6 +10,31 @@ import UIKit
 
 class RegisterViewController: UIViewController {
 
+    
+    
+    
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    
+    
+    
+    @IBOutlet weak var userTextField: UITextField!
+    
+
+    
+    
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -26,6 +51,39 @@ class RegisterViewController: UIViewController {
  
     
     @IBAction func uploadButton(_ sender: UIBarButtonItem) {
+        
+     print("You Click Upload")
+     
+ //      Get Value From TextField
+        
+        
+        
+        
+        let name = nameTextField.text!
+        let user = userTextField.text!
+        let password = passwordTextField.text!
+        
+ //       Show Massage on Console
+        print("name==>>\(name)")
+        print("user==>>\(user)")
+        print("password==>>\(password)")
+        
+   //     Check Space
+        
+        if ((name.count == 0) || (user.count == 0) || (password.count == 0)) {
+            print("Have Space")
+        }else{
+            print("NO Space")
+        }
+        
+        
+        
+        
+        
+    
+  
+        
+        
     }
     
     
